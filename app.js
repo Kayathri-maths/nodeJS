e1df912fc647a8dee86775eac52b527e37868bdd
Kayathri-maths <kayualagarsamy@gmail.com>
2024-01-06 19:57:02 +0530

    Serving HTML , CSS and JS (VIEWS)

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+const path = require('path');
+
 const express = require('express');
-
-const app = express();
 const bodyParser = require('body-parser');
-const loginRoutes = require('./routes/login');
-const msgRoutes = require('./routes/msg');
 
-app.use(bodyParser.urlencoded({extended : false}));
+const app= express();
 
-app.use(loginRoutes);
-app.use(msgRoutes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const contactRoutes = require('./routes/contact');
+const successRoutes =require('./routes/success');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use(contactRoutes);
+app.use(successRoutes);
+
+
+app.use('/',( req , res , next) => {
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
 app.listen(3000);
+
