6c32d0309c286afd783ddf19d275a07365d5941a
Kayathri-maths <kayualagarsamy@gmail.com>
2024-01-10 11:33:14 +0530

    Dynamic Routes Part 1

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ const p = path.join(
     'products.json'
   );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
       fs.readFile(p, (err, fileContent) => {
         if (err) {
           return  cb([]);
@@ -18,11 +18,15 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title,imageUrl,description,price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+   this.id = Math.random().toString();
    getProductsFromFile( products => {
     products.push(this);
     fs.writeFile(p, JSON.stringify(products), err => {
@@ -34,4 +38,12 @@ module.exports = class Product {
   static fetchAll(cb) {
       getProductsFromFile(cb);
   }
+
+
+ static findById(id, cb) {
+  getProductsFromFile(products => {
+    const product = products.find(p => p.id === id);
+    cb(product);
+  });
+}
 };
