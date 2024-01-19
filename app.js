f6258e06cbf758aeecc0976d4f7a9a2b8e22861b
Kayathri-maths <kayualagarsamy@gmail.com>
2024-01-19 10:39:27 +0530

    Starting with MYSQL

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app= express();
 
@@ -12,11 +13,15 @@ app.set('views','views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products');
 // const contactRoutes = require('./routes/contact');
 // const successRoutes =require('./routes/success');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
+
+
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
